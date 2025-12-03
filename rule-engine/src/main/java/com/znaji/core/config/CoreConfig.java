cb3fca7a5b2b6bc63d1eea1bfe483a441cbe53ce
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-12-03 17:55:37 +0100

    feat(rule-core): config class to enable component scan

---
+++ rule-engine/src/main/java/com/znaji/core/config/CoreConfig.java
@@ -0,0 +1,12 @@
+package com.znaji.core.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {
+        "com.znaji.core",
+        "com.znaji.rules"
+})
+public class CoreConfig {
+}
