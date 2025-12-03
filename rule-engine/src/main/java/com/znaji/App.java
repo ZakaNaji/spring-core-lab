921ff81f0c09aaeeaa060a9386251ecbb460a170
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-12-03 17:55:46 +0100

    feat(rule-core): demo

--- rule-engine/src/main/java/com/znaji/App.java
+++ rule-engine/src/main/java/com/znaji/App.java
@@ -1,5 +1,11 @@
 package com.znaji;
 
+import com.znaji.core.RuleContext;
+import com.znaji.core.RuleEngine;
+import com.znaji.core.RuleResult;
+import com.znaji.core.config.CoreConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try (var context = new AnnotationConfigApplicationContext()) {
+            context.register(CoreConfig.class);
+            context.refresh();
+
+            RuleContext ruleContext = new RuleContext();
+            ruleContext.put("text", "    test        ");
+            //Rule engine:
+            RuleEngine engine = context.getBean(RuleEngine.class);
+
+            RuleResult trimResult = engine.execute("trim", ruleContext);
+            System.out.println(trimResult.getOutput());
+
+            RuleResult toUpperResult = engine.execute("to-upper", ruleContext);
+            System.out.println(toUpperResult.getOutput());
+        }
     }
 }
