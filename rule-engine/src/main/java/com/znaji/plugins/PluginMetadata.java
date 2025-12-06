2e4d78377ac558357d29319e635716ca064a906d
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-12-06 15:31:08 +0100

    feat(rule-plugin-loader): add lastModified field to not reload old plugins, used by the watcher to decide if a plug was changed and needs to be re-loaded

--- rule-engine/src/main/java/com/znaji/plugins/PluginMetadata.java
+++ rule-engine/src/main/java/com/znaji/plugins/PluginMetadata.java
@@ -1,17 +1,34 @@
 package com.znaji.plugins;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class PluginMetadata {
 
     private final Path jarPath;
+    private final long lastModified;
 
     public PluginMetadata(Path jarPath) {
         this.jarPath = jarPath;
+        try {
+            this.lastModified = Files.getLastModifiedTime(jarPath).toMillis();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read lastModified for " + jarPath, e);
+        }
     }
 
     public Path getJarPath() {
         return jarPath;
+    }
+
+    public long getLastModified() {
+        return lastModified;
+    }
+
+    public String getId() {
+        // simple identity: jar file name
+        return jarPath.getFileName().toString();
     }
 
     public String getJarName() {
